3c7e87833c53f4a898b8e812d06ac4dab58c4579
ary <ary@hyperbeam.sh>
2026-03-07 23:11:57 -0800

    make dark

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import { Canvas, T } from '@threlte/core'
 	import { Billboard, OrbitControls, Text } from '@threlte/extras'
-	export let data
+	let { data } = $props()
 </script>
 
-<div class="w-full h-screen">
+<div class="w-full h-screen bg-black">
 	<Canvas>
 		<T.OrthographicCamera makeDefault position={[data.camera_init_pos.x, data.camera_init_pos.y, data.camera_init_pos.z]}>
 			<OrbitControls target={[data.centroid.x, data.centroid.y, data.centroid.z]} />
@@ -12,9 +12,9 @@
 
 		<T.AmbientLight intensity={1} />
 
-		{#each data.points as point} -->
-    		<Billboard position={[point.x, point.y, point.z]}>
-                <Text text="+" fontSize={15} color="black" />
+		{#each data.points as point}
+			<Billboard position={[point.x, point.y, point.z]}>
+                <Text text="+" fontSize={15} {color} />
             </Billboard>
 		{/each}
 	</Canvas>
