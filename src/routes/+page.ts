2685393f769f003d05d70d91b98604de58bedf2b
ary <ary@hyperbeam.sh>
2026-03-10 19:14:31 -0700

    update preprocess script and data
    add headers to matrix/embedding files, update scripts to handle this, and include model name in point struct

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -6,9 +6,9 @@ export const load = async ({ fetch }) => {
   const res = await fetch("/embedding2.csv")
   const text = await res.text()
 
-  const points = text.trim().split('\n').map(row => {
-    const [x, y, z] = row.split(',').map(n => CLOUD_SCALE * Number(n))
-    return { x, y, z }
+  const points = text.trim().split('\n').slice(1).map(row => {
+    const [model, x, y, z] = row.split(',')
+    return { model, x: CLOUD_SCALE * Number(x), y: CLOUD_SCALE * Number(y), z: CLOUD_SCALE * Number(z) }
   })
 
   const sum_of_points = points.reduce(
@@ -26,6 +26,7 @@ export const load = async ({ fetch }) => {
   }
 
   const centered_points = points.map(p => ({
+    model: p.model,
     x: p.x - centroid.x,
     y: p.y - centroid.y,
     z: p.z - centroid.z,
